ba691b5fb7dac85213a1c03a5c46a8a20b8434ef
Pann Cherry <panncherry@Panns-MacBook-Pro.local>
2018-08-27 22:07:16 -0400

    Here is my commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Pre-work - TipCalculator
+
+TipCalculator is a tip calculator application for iOS.
+
+Submitted by: Pann Cherry
+
+Time spent: 3.5 hours spent in total
+
+## User Stories
+
+The following **required** functionality is complete:
+
+* [ ] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
+
+The following **optional** features are implemented:
+* [ ] Remembering the bill amount across app restarts (if <10mins)
+* [ ] Using locale-specific currency and currency thousands separators.
+
+
+The following **additional** features are implemented:
+
+- [ ] Use UISlider to calculate tip amount and total amount per person for sharing the bill.
+
+## Video Walkthrough 
+
+Here's a walkthrough of implemented user stories: 
+
+<img src='https://i.imgur.com/did6xqN.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+It was challenging to customize the UISegmentedControl apperearnce.
+
+## License
+
+    Copyright [2018] [Pann Cherry]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPerPersonLabel: UILabel!
+    @IBOutlet weak var totalPerPersonLabel: UILabel!
+    @IBOutlet weak var tipSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numOfPerson: UILabel!
+    @IBOutlet weak var numOfPersonSlider: UISlider!
+    
+    var currentSliderValue = Int(1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //convert currency to double
+    func convertCurrencyToDouble(input: String) -> Double? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale.current
+        return numberFormatter.number(from: input)?.doubleValue
+    }
+    
+    //convert double to currency
+    func convertDoubleToCurrency(amount: Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale.current
+        return numberFormatter.string(from: NSNumber(value: amount))!
+    }
+    
+    //use Tap Gesture Recognition to dismiss the keyboard when tapping anywhere on the screen
+    @IBAction func onTapGesture(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    //call "view changed event" to update the numOfPerSonLabel
+    @IBAction func personSlider(_ sender: Any) {
+        currentSliderValue = Int(numOfPersonSlider.value)
+        numOfPerson.text = ("\(currentSliderValue)")
+        self.calculateTip()
+    }
+    
+    //use Editing changed event to calculate tip using user input amount
+    //inorder to get updated value according to the selected segmented control value, use "view changed" events and connect it with the calculateTip function below.
+    @IBAction func calculateTip(_ sender: Any? = nil) {
+        //convert the input string to double
+        //if invalid input and cannot convert to double, return zero
+        let bill = Double(billTextField.text!) ?? 0
+        let tipPercentage = [0.10, 0.15, 0.20]
+        let tip = bill * tipPercentage[tipSegmentedControl.selectedSegmentIndex]
+        let total = tip + bill
+        let tipPerPerson = tip/Double(currentSliderValue)
+        let totalPerPerson = total/Double(currentSliderValue)
+        
+        //you can also use String(format: String, any) to put $ sign manually and set to two decimal. Example: tipLabel.text = String(format: "$%.2f", tip)
+        tipLabel.text = convertDoubleToCurrency(amount: tip)
+        totalLabel.text = convertDoubleToCurrency(amount: total)
+        tipPerPersonLabel.text = convertDoubleToCurrency(amount: tipPerPerson)
+        totalPerPersonLabel.text = convertDoubleToCurrency(amount: totalPerPerson)
+    }
 }
-
